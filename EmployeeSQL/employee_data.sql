44bb1188819eb5473a7d441a3b3878a4f09b8f6e
DominicT1995 <domedt@gmail.com>
2024-02-28 19:43:25 -0600

    employee queries file created for data analysis and completed, employee data filed cleaned up

--- EmployeeSQL/employee_data.sql
+++ EmployeeSQL/employee_data.sql
@@ -1,21 +1,19 @@
+--Create tables for given .csv files in the Resources folder--
+
 CREATE TABLE titles (
 	title_id VARCHAR(20) PRIMARY KEY,
 	title VARCHAR(20) NOT NULL
 
 );
-
-SELECT * FROM titles;
------------------------------------------------------------
+-------------------------------------------------------------
 CREATE TABLE departments (
 	dept_no VARCHAR(20) PRIMARY KEY,
 	dept_name VARCHAR(20) NOT NULL
 
 );
-
-SELECT * FROM departments;
------------------------------------------------------------
+-------------------------------------------------------------
 CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
+	emp_no INT PRIMARY KEY NOT NULL,
 	emp_title_id VARCHAR(20) NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date VARCHAR(20) NOT NULL,
@@ -25,34 +23,26 @@ CREATE TABLE employees (
 	hire_date VARCHAR(20) NOT NULL
 
 );
-
-SELECT * FROM employees;
------------------------------------------------------------
+-------------------------------------------------------------
 CREATE TABLE salaries (
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	salary INT
+	salary INT NOT NULL
 
 );
-
-SELECT * FROM salaries;
------------------------------------------------------------
+-------------------------------------------------------------
 CREATE TABLE dept_emp (
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	dept_no VARCHAR(20) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 
 );
-
-SELECT * FROM dept_emp;
------------------------------------------------------------
+-------------------------------------------------------------
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(20) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	emp_no INT,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 
 );
-
-SELECT * FROM dept_manager;
